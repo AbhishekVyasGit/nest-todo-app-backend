905b682989604aa8f24a60449f955d5a68f4a899
abhi <abhishekvyas30008@gmail.com>
2024-07-04 11:02:07 +0530

    update environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: [".prod.env"] }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: [".env"] }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT, 10));
 }
 bootstrap();
